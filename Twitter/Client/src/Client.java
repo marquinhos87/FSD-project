44d0415430a1bf07738e525c021077e5891666e2
João Marques <joaomarques@MacBook-Pro-do-Marques.local>
2019-11-18 18:09:55 +0000

    Inicio do cliente

--- Twitter/Client/src/Client.java
+++ Twitter/Client/src/Client.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
+        if(args.length < 2) {
 
+        }
+        else {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader((System.in)));
+                Socket s = new Socket(args[0],Integer.parseInt(args[1]));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- Twitter/Server/src/Server.java
+++ Twitter/Server/src/Server.java
@@ -1,3 +1,5 @@
+import java.net.ServerSocket;
+
 public class Server {
 
     public static void main(String[] args) {
