4a545381a1011fa23ae661ab48d80d258fe76b83
Alberto Faria <albertofaria.dev@gmail.com>
2019-12-13 18:48:50 +0000

    Informative commit message.

--- chirper/server/src/main/java/chirper/server/ServerId.java
+++ chirper/server/src/main/java/chirper/server/ServerId.java
@@ -5,6 +5,8 @@ package chirper.server;
 /* -------------------------------------------------------------------------- */
 
 /**
+ * TODO: document
+ *
  * Just a wrapper around an int. Exists for type safety.
  *
  * Instances of this class are immutable.
@@ -13,16 +15,32 @@ public class ServerId implements Comparable< ServerId >
 {
     private final int value;
 
+    /**
+     * TODO: document
+     *
+     * @param value TODO: document
+     */
     public ServerId(int value)
     {
         this.value = value;
     }
 
+    /**
+     * TODO: document
+     *
+     * @return TODO: document
+     */
     public int getValue()
     {
         return this.value;
     }
 
+    /**
+     * TODO: document
+     *
+     * @param other TODO: document
+     * @return TODO: document
+     */
     @Override
     public int compareTo(ServerId other)
     {
