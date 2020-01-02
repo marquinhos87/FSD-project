ca5caa2feec6e6e095b06ed88fed1b839871335d
Alberto Faria <albertofaria.dev@gmail.com>
2020-01-02 11:54:47 +0000

    Informative commit message.

--- chirper/shared/src/main/java/chirper/shared/ChirpStore.java
+++ chirper/shared/src/main/java/chirper/shared/ChirpStore.java
@@ -1,14 +1,14 @@
 package chirper.shared;
 
 /**
- * Just a simple chirp store.
+ * Just a simple persistent chirp store.
  *
  * Add chirps with appendChirp(). The chirps end up having the same order as the
  * order in which appendChirp() is invoked.
  */
-public class PersistentState
+public class ChirpStore
 {
-    public PersistentState()
+    public ChirpStore()
     {
 
     }
